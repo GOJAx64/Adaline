1fd9a9d66de85bc1a83bddf955ad5475b8f8bb5d
Eliseo ortiz <eliseo271099@gmail.com>
2022-02-23 18:53:48 -0600

    seguimiento

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from turtle import forward, right
+from matplotlib import markers
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import TextBox, Button
@@ -22,17 +23,26 @@ class Ventana:
     linea=None
     texto_de_epoca = None
     termino=False
+    errores=[]
+    total=0
+    deseada_deseada=0
+    deseada_nodeseada=0
+    nodeseada_deseada=0
+    nodeseada_nodeseada=0
 
     def __init__(self):
         #Configuracion inicial de la interfaz grafica.
         mpl.rcParams['toolbar'] = 'None'
-        self.fig, self.grafica = plt.subplots()
+        self.fig, (self.grafica, self.grafica_errores) = plt.subplots(2, 1)
         self.fig.canvas.manager.set_window_title('Adaline')
         self.fig.set_size_inches(10, 8, forward=True)
         plt.subplots_adjust(bottom=0.150, top=0.850)
         self.grafica.set_xlim(-1.0,1.0)
         self.grafica.set_ylim(-1.0,1.0)
         self.fig.suptitle("Algoritmo del Adaline")
+        self.grafica_errores.set_title("Errores")
+        self.grafica_errores.set_xlabel("Epoca")
+        self.grafica_errores.set_ylabel("Errores")
     # Acomodo de los botones y cajas de texto
         cordenadas_rango = plt.axes([0.200, 0.9, 0.100, 0.03])
         coordenadas_epcoas = plt.axes([0.440, 0.9, 0.100, 0.03])
@@ -41,7 +51,7 @@ class Ventana:
         coordenadas_evaluar = plt.axes([0.160, 0.05, 0.1, 0.03])
         coordenadas_reiniciar = plt.axes([0.270, 0.05, 0.1, 0.03])
         coordenadas_entrenar_adaline = plt.axes([0.680, 0.05, 0.1, 0.03])
-        #coordenadas_entrenar_perceptron = plt.axes([0.800, 0.05, 0.1, 0.03])
+        coordenadas_entrenar_perceptron = plt.axes([0.800, 0.05, 0.1, 0.03])
         self.text_box_rango = TextBox(cordenadas_rango, "Rango de aprendizaje:")
         self.text_box_epocas = TextBox(coordenadas_epcoas, "Épocas maximas:")
         self.text_box_error_minimo_deseado = TextBox(coordenadas_error_deseado, "Error mínimo deseado:")
@@ -49,7 +59,7 @@ class Ventana:
         boton_evaluar = Button(coordenadas_evaluar, "Evaluar")
         boton_reiniciar = Button(coordenadas_reiniciar, "Reiniciar")
         boton_entrenar_adaline = Button(coordenadas_entrenar_adaline, "Adaline")
-        # boton_entrenar_perceptron = Button(coordenadas_entrenar, "Perceptron")
+        boton_entrenar_perceptron = Button(coordenadas_entrenar_perceptron, "Perceptron")
         self.text_box_epocas.on_submit(self.validar_epocas)
         self.text_box_rango.on_submit(self.validar_rango)
         self.text_box_error_minimo_deseado.on_submit(self.validar_error_minimo_deseado)
@@ -57,7 +67,7 @@ class Ventana:
         boton_evaluar.on_clicked(self.evaluar)
         boton_reiniciar.on_clicked(self.reiniciar)
         boton_entrenar_adaline.on_clicked(self.entrenar_adaline)
-        #boton_entrenar_perceptron.on_clicked(self.entrenar_perceptron)
+        boton_entrenar_perceptron.on_clicked(self.entrenar_perceptron)
         self.fig.canvas.mpl_connect('button_press_event', self.__onclick)
         plt.show()
 
@@ -78,22 +88,55 @@ class Ventana:
     def entrenar_adaline(self, event):
         if self.pesos_inicializados and not self.adaline_entrenado:
             error_cuadratico = 1
-            while error_cuadratico > self.error_minimo and self.epoca_actual < self.adaline.epocas_maximas:
+            self.total=len(self.puntos)
+            while error_cuadratico > self.error_minimo and self.epoca_actual < self.adaline.epocas_maximas:   
+                self.deseada_deseada=0
+                self.deseada_nodeseada=0
+                self.nodeseada_deseada=0
+                self.nodeseada_nodeseada=0
                 self.epoca_actual += 1
+                error_cuadratico = 0
                 for i, x in enumerate(self.puntos):
                     x = np.insert(x, 0, -1.0)
-                    error = self.clase_deseada[i] - self.adaline.f(x)
-                    derivada = self.adaline.f(x) * ( 1- self.adaline.f(x))
-                    self.adaline.pesos = self.adaline.pesos + np.multiply((2*self.adaline.rango * error * derivada ), x)
-                    #error cuadratico
+                    y=self.adaline.f(x)
+                    error = self.clase_deseada[i] - y
+                    if(self.clase_deseada[i]==1):
+                        if(y>.5):#deseada y deseada
+                            self.deseada_deseada+=1
+                        else:
+                            self.deseada_nodeseada+=1
+                    else:
+                        if(y>.5):#nodeseada y deseada
+                            self.nodeseada_deseada+=1
+                        else:
+                            self.nodeseada_nodeseada+=1
+                    derivada = y * ( 1- y)
+                    diferencial=2*self.adaline.rango * error * derivada
+                    error_cuadratico+=error**2
+                    self.adaline.pesos = self.adaline.pesos + np.multiply((diferencial ), x)
                     self.graficar_linea()
+                self.errores.append(error_cuadratico)
+                self.graficar_errores()
             
-            self.grafica.text(0, -1.250,'Resultado = ' + ('Converge' if self.termino else 'No converge'),fontsize=16) #cambiar validacion
+            if(error_cuadratico < self.error_minimo):
+                self.termino=True
+            self.grafica.text(0, -1.250,'Resultado = ' + ('Error minimo obtenido' if self.termino else 'No converge'),fontsize=16) #cambiar validacion
             self.texto_de_epoca.set_text("Épocas: %s" % self.epoca_actual)
             plt.pause(0.1)
             self.adaline_entrenado = True
+            print("Matriz de confusion")
+            print("N="+str(self.total))
+            print("\t\tObtenida si\tObtenida no")
+            print("Deseada si\t"+str(self.deseada_deseada)+"\t\t"+str(self.deseada_nodeseada))
+            print("Deseada No\t"+str(self.nodeseada_deseada)+"\t\t"+str(self.nodeseada_nodeseada))
+            print("\t\t"+str(self.deseada_deseada+self.nodeseada_deseada)+"\t\t"+str(self.deseada_nodeseada+self.nodeseada_nodeseada))
     
-
+    def graficar_errores(self):
+        x=self.errores
+        y = range(len(x))
+        self.grafica_errores.clear()
+        self.grafica_errores.plot(y,x)
+        plt.pause(0.3)
     def evaluar(self,event):
         if(self.adaline_entrenado and len(self.sin_evaluar)>0):
             self.grafica.clear() 
@@ -109,7 +152,7 @@ class Ventana:
             
             for  i,x in enumerate(self.sin_evaluar):
                 x = np.insert(x, 0, -1.0)
-                self.grafica.plot(x[1], x[2], 'gx' if self.adaline.pw(x) else 'g.')
+                self.grafica.plot(x[1], x[2], 'gx' if self.adaline.f(x)>.5 else 'g.')
            
             plt.pause(0.1)
 
@@ -195,21 +238,30 @@ class Ventana:
         self.text_box_rango.set_val('')
         self.text_box_epocas.set_val('')
         self.text_box_error_minimo_deseado.set_val('')
+        self.errores=[]
+        self.grafica_errores.clear()
 
 
     def barrido(self):
         x=-1
-        y=1
+        y=0
         while(y>=-1):
             x=-1
             while x<=1:
                 p=[x,y]
                 j = np.insert(p, 0, -1.0)
-                self.grafica.plot(x,y, 'r.' if self.adaline.pw(j) else 'b.')
+                alpha=self.adaline.f(j)
+                if (alpha>.5):
+                    self.grafica.plot(x,y, color=(1,0,0,alpha),marker='.' )
+                else:
+                    self.grafica.plot(x,y, color=(0,0,1,alpha),marker='.' )
+                
                 x+=.01
             y-=.01
             print(y)
             
+    def entrenar_perceptron(self):
+        pass
 
 if __name__ == '__main__':
     Ventana()
