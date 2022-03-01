062dc1260ff0efb9ba7c602c4d8ff7e9f04afa51
AlexDominguez18 <oscar.dominguez4686@alumnos.udg.mx>
2022-03-01 00:34:55 -0600

    Reemplazando la impresión del resultado por el error cuadratico

--- Main.py
+++ Main.py
@@ -126,7 +126,7 @@ class Ventana:
             
             if(error_cuadratico < self.error_minimo):
                 self.termino=True
-            plt.text(1.2, 0.3,'Resultado = ' + ('Error minimo obtenido' if self.termino else 'No converge'), fontsize=16) 
+            plt.text(1.2, 0.3, 'Error cuadrático = ' + str(error_cuadratico), fontsize=16) 
             self.texto_de_epoca.set_text("Épocas: %s" % self.epoca_actual)
             plt.pause(0.1)
             self.adaline_entrenado = True
